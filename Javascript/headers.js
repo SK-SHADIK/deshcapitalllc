22f026c868d6964618ad265b0321d402fafd4013
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-03-25 17:39:19 +0600

    Fixed Nav Bar

--- Javascript/headers.js
+++ Javascript/headers.js
@@ -1,17 +1,16 @@
-const menuBtn = document.querySelector(".menu-icon span");
+const menuButton = document.querySelector(".menu-icon span");
         const cancelButton = document.querySelector(".cancel-btn");
         const navbaritems = document.querySelector(".nav-bar");
-        menuBtn.onclick = () => {
+        menuButton.onclick = () => {
             navbaritems.classList.add("active");
-            menuBtn.classList.add("hide");
+            menuButton.classList.add("hide");
             cancelButton.classList.add("show");
         }
         
         cancelButton.onclick = () => {
             navbaritems.classList.remove("active");
-            menuBtn.classList.remove("hide");
+            menuButton.classList.remove("hide");
             cancelButton.classList.remove("hide");
             cancelButton.classList.remove("show");
-            cancelButton.style.color = "#ff3d00";
         }
         
