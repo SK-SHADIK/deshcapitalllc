a0756b8f6bd107e69e20ec26135110bc952e8fda
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-03-25 17:09:48 +0600

    Fixed Nav Bar

--- Javascript/headers.js
+++ Javascript/headers.js
@@ -1,24 +1,17 @@
 const menuBtn = document.querySelector(".menu-icon span");
-        const searchBtn = document.querySelector(".search-icon");
-        const cancelBtn = document.querySelector(".cancel-icon");
-        const items = document.querySelector(".nav-items");
-        const form = document.querySelector("form");
+        const cancelButton = document.querySelector(".cancel-btn");
+        const navbaritems = document.querySelector(".nav-bar");
         menuBtn.onclick = () => {
-            items.classList.add("active");
+            navbaritems.classList.add("active");
             menuBtn.classList.add("hide");
-            searchBtn.classList.add("hide");
-            cancelBtn.classList.add("show");
+            cancelButton.classList.add("show");
         }
-        cancelBtn.onclick = () => {
-            items.classList.remove("active");
+        
+        cancelButton.onclick = () => {
+            navbaritems.classList.remove("active");
             menuBtn.classList.remove("hide");
-            searchBtn.classList.remove("hide");
-            cancelBtn.classList.remove("show");
-            form.classList.remove("active");
-            cancelBtn.style.color = "#ff3d00";
+            cancelButton.classList.remove("hide");
+            cancelButton.classList.remove("show");
+            cancelButton.style.color = "#ff3d00";
         }
-        searchBtn.onclick = () => {
-            form.classList.add("active");
-            searchBtn.classList.add("hide");
-            cancelBtn.classList.add("show");
-        }
+        
