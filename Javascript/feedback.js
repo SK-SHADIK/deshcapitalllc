c336b049ea91b1918cedbf5bd4324e6e0ccd1f08
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-04-07 08:47:59 +0600

    Footer Change

--- Javascript/feedback.js
+++ Javascript/feedback.js
@@ -3,7 +3,7 @@ let form = document.querySelector("form");
     form.addEventListener('submit', (e) => {
       e.preventDefault();
       let data = new FormData(form);
-      fetch('https://script.google.com/macros/s/AKfycbxTiY2hFjr9LRBEQxWZo2YJb-KD_m28PFvWPAcKGtFduLd-3im4ZJodkQfXnJ_mCfgB/exec', {
+      fetch('https://script.google.com/macros/s/AKfycbz0p0R-NYtQM-SARyP4w7ZYGNoMfcRrT7rtdowzD3EK-6RRAnGXBGyNXyvTAD1Y2I_w/exec', {
         method: "POST",
         body: data
       })
