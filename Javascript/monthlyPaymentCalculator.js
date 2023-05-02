066a0bcf6c994c3251d6714e2d8ef0533f51d05b
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-05-02 22:37:05 +0600

    Monthly Payment Calculator

--- Javascript/monthlyPaymentCalculator.js
+++ Javascript/monthlyPaymentCalculator.js
@@ -5,5 +5,5 @@ function calculateMonthlyPayment() {
 
     let monthlyPayment = loanAmount * ((interestRate / 1200) / (1 - Math.pow(1 + (interestRate / 1200), -loanTerm * 12)));
 
-    document.getElementById("monthlyPayment").innerHTML = "Your monthly payment is $" + monthlyPayment.toFixed(2) + ".";
+    document.getElementById("monthlyPayment").innerHTML = "You will need to pay $" + monthlyPayment.toFixed(2) + " every month in next " + loanTerm + " years.";
   }
