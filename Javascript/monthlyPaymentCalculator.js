1470d54f857fdeda49ee5d6557719223bb12f86f
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-05-03 21:17:12 +0600

    Footer Change

--- Javascript/monthlyPaymentCalculator.js
+++ Javascript/monthlyPaymentCalculator.js
@@ -1,9 +1,9 @@
 function calculateMonthlyPayment() {
-    let loanAmount = document.getElementById("loanAmount").value;
-    let loanTerm = document.getElementById("loanTerm").value;
-    let interestRate = document.getElementById("interestRate").value;
+  let loanAmount = document.getElementById("loanAmount").value;
+  let loanTerm = document.getElementById("loanTerm").value;
+  let interestRate = document.getElementById("interestRate").value;
 
-    let monthlyPayment = loanAmount * ((interestRate / 1200) / (1 - Math.pow(1 + (interestRate / 1200), -loanTerm * 12)));
+  let monthlyPayment = loanAmount * ((interestRate / 1200) / (1 - Math.pow(1 + (interestRate / 1200), -loanTerm * 12)));
 
-    document.getElementById("monthlyPayment").innerHTML = "You will need to pay $" + monthlyPayment.toFixed(2) + " every month in next " + loanTerm + " years.";
-  }
+  document.getElementById("monthlyPayment").innerHTML = "You will need to pay $" + monthlyPayment.toFixed(2) + " every month in next " + loanTerm + " years.";
+}
